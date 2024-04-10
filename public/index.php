753bccfef3639f8d626221c73cb6e9dab7778060
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-04-10 12:41:37 -0300

    Tela de login

--- public/index.php
+++ public/index.php
@@ -1,13 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-$user = new User(['name' => 'Maria', 'email' => 'maria@gmail.com']);
-
-print_r(User::get(['id' => 1], 'name, email'));
-
-foreach(User::get([], 'name') as $user) {
-  echo $user->name;
-  echo '<br>';
-}
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
