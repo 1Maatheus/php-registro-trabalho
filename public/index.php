3e117c9af2059b7ac7294b1b775a604bcb28701b
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-04-10 12:23:38 -0300

    Construindo método get

--- public/index.php
+++ public/index.php
@@ -4,4 +4,10 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Maria', 'email' => 'maria@gmail.com']);
-echo $user->getSelect();
+
+print_r(User::get(['id' => 1], 'name, email'));
+
+foreach(User::get([], 'name') as $user) {
+  echo $user->name;
+  echo '<br>';
+}
