8be1d2d095385efbb98f403f5681297519b63bdd
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-04-10 12:14:59 -0300

    Tratando consultas

--- public/index.php
+++ public/index.php
@@ -4,7 +4,4 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Maria', 'email' => 'maria@gmail.com']);
-$user->email = 'maria1@gmail.com';
-print_r($user);
-echo '<br>';
-print_r($user->email);
+echo $user->getSelect();
