07d7e5c73e76e2640b296c9d1738972fcf69b1e5
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-04-10 11:59:31 -0300

    Atualizando código

--- public/index.php
+++ public/index.php
@@ -4,5 +4,7 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Maria', 'email' => 'maria@gmail.com']);
+$user->email = 'maria1@gmail.com';
 print_r($user);
-echo 'Fim!';
+echo '<br>';
+print_r($user->email);
