85583e1ea64747192ab7e512686ed78562c56b34
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-04-11 11:53:01 -0300

    Adicionando header

---
+++ src/views/template/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/comum.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/icofont.min.css">
+  <link rel="stylesheet" href="assets/css/template.css">
+  <title>Sistema de Ponto</title>
+</head>
+<body>
+  <header class="header">
+    <div class="logo">
+      <i class="icofont-travelling mr-2"></i>
+      <span class="font-weight-light">Ponto</span>
+      <span class="font-weight-bold mx-2">S</span>
+      <span class="font-weight-light">eguro</span>
+    </div>
+    <div class="menu-toggle mx-3">
+      <i class="icofont-navigation-menu"></i>
+    </div>
+    <div class="spacer">
+    </div>
+  </header>
