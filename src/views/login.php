4825a2ca88ddc0fcf57b4b5bb45df6d31e361991
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-04-11 11:34:44 -0300

    Personalizando as exceptions

--- src/views/login.php
+++ src/views/login.php
@@ -21,11 +21,17 @@
         <?php include(TEMPLATE_PATH . '/messages.php') ?>
         <div class="form-group">
           <label for="email">E-mail</label>
-          <input type="email" id="email" name="email" class="form-control" value="<?= $email?>" placeholder="email@exemplo.com" autofocus required>
+          <input type="email" id="email" name="email" class="form-control <?= $errors['email'] ? 'is-invalid' : '' ?>" value="<?= $email?>" placeholder="email@exemplo.com" autofocus>
+          <div class="invalid-feedback">
+            <?= $errors['email'] ?>
+          </div>
         </div>
         <div class="form-group">
           <label for="password">Senha</label>
-          <input type="password" id="password" name="password" class="form-control" placeholder="Informe a sua senha">
+          <input type="password" id="password" name="password" class="form-control <?= $errors['password'] ? 'is-invalid' : '' ?>" placeholder="Informe a sua senha">
+          <div class="invalid-feedback">
+            <?= $errors['password'] ?>
+        </div>
         </div>
       </div>
       <div class="card-footer">
