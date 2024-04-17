3d9e2b5b4b82c056e65aac1c5bb2bb81ccd96d15
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-04-17 12:55:20 -0300

    Configurando controller

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -2,7 +2,12 @@
 session_start();
 requireValidSession();
 
+loadModel('Workinghours');
+
 $date = (new Datetime())->getTimeStamp();
 $today = date('d/m/Y', $dat);
 
-loadTemplateView('day_records', ['today' => $today]);
+$user = $_SESSION['user'];
+$records = Workinghours::loadFromUserAndDate($user->id, date('Y-m-d'));
+
+loadTemplateView('day_records', ['today' => $today, 'records' => $records, 'user' => $user, 'success' => $_SESSION['message'], 'error' => $_SESSION['error']]);
