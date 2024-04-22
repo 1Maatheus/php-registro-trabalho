1f48153121663ec1a3fddf66428839974ba4b9f5
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-04-22 13:14:23 -0300

    Commit

--- src/controllers/teste.php
+++ src/controllers/teste.php
@@ -1,10 +1,1 @@
 <?php
-//Controller temporário
-$i1 = DateInterval::createFromDateString('9 hours');
-$i2 = DateInterval::createFromDateString('6 hours');
-
-echo sumIntervals($i1, $i2)->format('%H:%I:%S');
-
-echo '<br>';
-
-
